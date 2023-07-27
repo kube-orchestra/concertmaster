59923a64b1abf7d2c18833867f67310e702f90e3
Amador Pahim <apahim@redhat.com>
2023-07-27 09:20:28 +0100

    Adding Kubernetes manifests
    This is needed to deploy concertmaster
    
    Signed-off-by: Amador Pahim <apahim@redhat.com>

--- build_deploy.sh
+++ build_deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 CGO_ENABLED=0 go build ./cmd/concertmaster
-docker build -t quay.io/apahim/concertmaster:v2 .
-docker push quay.io/apahim/concertmaster:v2
+docker build -t quay.io/apahim/concertmaster:v13 .
+docker push quay.io/apahim/concertmaster:v13
